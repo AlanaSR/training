8723296ff6fbb9c974e73ee8d22013df9dc982f6
AlanaSR <svetlanka.shkola@gmail.com>
2024-06-20 12:53:56 +0500

    did all exs

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,42 @@ public class Main {
         short flowers = 4899;
         short card = 157;
         byte sale = 10;
-        int purchaseWithSale = (flowers + card) * (100-sale)/100;
-        int saleRub = (flowers + card) * sale / 100;
+        float purchaseWithSale = (flowers + card) * (100f-sale)/100f;
+        float saleRub = (flowers + card) * sale / 100f;
         System.out.println("Сумма двух покупок с учетом скидки равна " + purchaseWithSale + " рублей, скидка составила " + saleRub + " рублей.");
 
+        System.out.println("Задание 5");
+        int orangesPrice = 73;
+        int saleOranges = 15;
+        int bananasPrice = 95;
+        int orangesBuy = 10;
+        int bananasBuy = 2;
+        float orangesWithSale = orangesPrice * (100 - saleOranges) / 100f;
+        float totalSum1 = bananasPrice * bananasBuy + orangesBuy * orangesWithSale;
+        System.out.println("Сумма покупки с учетом скидки составила " + totalSum1 + " рублей.");
+
+        int champaign = 2870;
+        int champaignSale = 50;
+        int bread = 27;
+        int champaignBuy = 7;
+        int breadBuy = 1;
+        float champaignWithSale = champaign * (100-champaignSale)/100f;
+        float totalSum2 = champaignWithSale * champaignBuy + bread * breadBuy;
+        System.out.println("Сумма покупки с учетом скидки составила " + totalSum2 + " рублей.");
+
+        int champPrice = 436;
+        int whiteMushroomPrice = 578;
+        int whiteMushroomSale = 5;
+        float champBuy = 2.5f;
+        float whiteMushBuy = 3.75f;
+        float whiteMushWithSale = whiteMushroomPrice * (100 - whiteMushroomSale)/100f;
+        float totalSum3 = champPrice * champBuy + whiteMushWithSale * whiteMushBuy;
+        System.out.println("Сумма покупки с учетом скидки составила " + totalSum3 + " рублей.");
+
+        System.out.println("Задание 6");
+        double x = 0.40;
+        double y = 0.87;
+        System.out.println(11 * x + y);
+        System.out.println((x + 10 * y) - 15 * (x / y));
     }
 }
